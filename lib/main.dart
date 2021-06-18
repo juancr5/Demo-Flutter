b29a6e2ce3aa914376d7ce3a9f72dd322312dd80
juancr5 <juancr5@hotmail.com>
2021-06-17 22:39:48 -0500

    Upload Files

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'myapp.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+/*
+ * Or, do this in one single code line
+ * void main() =>  runApp(MyApp());
+ */
+
