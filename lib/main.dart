10a762643ba0a85015a784d6971a920185ea3735
juancr5 <juancr5@hotmail.com>
2021-07-01 23:03:42 -0500

    Update Avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'alert_page.dart';
+import 'avatar_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AlertPage(),
+      home: AvatarPage(),
     );
   }
 }
