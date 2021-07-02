837f47ae9b2e8d54efe810f4405c61acb6043625
juancr5 <juancr5@hotmail.com>
2021-07-02 18:25:22 -0500

    Update Animated Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'avatar_page.dart';
+import 'animated_container.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AvatarPage(),
+      home: AnimatedContainerPage(),
     );
   }
 }
