b14dfd93a21babf491115538a6354df724b23392
juancr5 <juancr5@hotmail.com>
2021-07-04 20:06:47 -0500

    Update Input

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'animated_container.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'input_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,11 +12,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Card Widget',
       debugShowCheckedModeBanner: false, //Quitar el logo de debug en la esquina superior
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English, no country code
+        const Locale('es', 'ES'), // Spanish, no country code
+      ],
 
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AnimatedContainerPage(),
+      home: InputPage(),
     );
   }
 }
